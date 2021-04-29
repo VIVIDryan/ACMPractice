dae62d8088bb13f4c7a6175a999af347958d78c2
VIVIDryan <cxqlove1999@outlook.com>
2021-04-29 21:34:24 +0800

    The first try

---
+++ AdvancedLanguage/AdvancedLanguage.cpp
@@ -0,0 +1,20 @@
+﻿// AdvancedLanguage.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double n;
+    double result;
+
+    cin >> n;
+    result = pow(abs(n), 0.5);
+    cout <<  setprecision(4) << fixed<<result;
+
+    return 0;
+}
+
